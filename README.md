d6c64190d39332d6f1c901885e5b6cb2c1c613ce
wanglele <wanglele@luojilab.com>
2025-11-11 20:40:49 +0800

    AutoMigrate user table

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-mx-shop-srvs
-    -user_srv 用户相关微服务 
-        -model 定义表结构
+mx-shop-srvs/  
+├── user_srv/                 # 用户相关微服务  
+├── model/                    # 定义表结构
+├──────── main/                
+├──────────────main.go        # 定义连接数据库，生成表结构相关内容
--- user_srv/model/user.go
+++ user_srv/model/user.go
@@ -22,6 +22,6 @@ type User struct {
 	Password string     `gorm:"type:varchar(100);not null;"`
 	NickName string     `gorm:"type:varchar(20)"`
 	Birthday *time.Time `gorm:"type:datetime"`
-	Gender   string     `gorm:"column:gender;type:varchar(6);default:male comment 'female表示女，male表示男'"`
-	Role     int        `gorm:"column:role;default:1;type:int comment '1表示普通用户，2表示管理员'"`
+	Gender   string     `gorm:"column:gender;type:varchar(6);default:'male';comment:'female表示女，male表示男'"`
+	Role     int        `gorm:"column:role;default:1;type:int;comment:'1表示普通用户，2表示管理员'"`
 }
