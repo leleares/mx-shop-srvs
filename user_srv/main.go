c2c79015c424cb98289ded1f1818040079f67226
wanglele <wanglele@luojilab.com>
2025-12-06 10:33:10 +0800

    Add global config

--- user_srv/main.go
+++ user_srv/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"mx-shop-srvs/user_srv/handler"
+	"mx-shop-srvs/user_srv/initialize"
 	"mx-shop-srvs/user_srv/proto"
 	"net"
 
@@ -14,6 +15,11 @@ func main() {
 	// flag处理的参数可在运行可执行文件时注入
 	IP := flag.String("ip", "0.0.0.0", "ip地址")
 	Port := flag.Int("port", 50051, "端口号")
+
+	// initialize
+	initialize.InitLogger()
+	initialize.InitConfig()
+	initialize.InitDB()
 
 	flag.Parse()
 	fmt.Println("ip", *IP)
