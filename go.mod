056baeb40fb201b218b310d6bb6357dadfc374f0
wanglele <wanglele@luojilab.com>
2025-12-07 13:45:30 +0800

    Add consul config center

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module mx-shop-srvs
 
-go 1.24.4
+go 1.25.3
 
 require (
 	github.com/anaskhan96/go-password-encoder v0.0.0-20201010210601-c765b799fd72
+	github.com/fsnotify/fsnotify v1.9.0
 	github.com/golang/protobuf v1.5.4
+	github.com/hashicorp/consul/api v1.33.0
 	google.golang.org/grpc v1.76.0
 	google.golang.org/protobuf v1.36.10
 	gorm.io/driver/mysql v1.6.0
@@ -12,8 +14,20 @@ require (
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
@@ -23,6 +37,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/exp v0.0.0-20250808145144-a408d31f581a // indirect
 )
 
 require (
--- user_srv/main.go
+++ user_srv/main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"mx-shop-srvs/user_srv/global"
 	"mx-shop-srvs/user_srv/handler"
 	"mx-shop-srvs/user_srv/initialize"
 	"mx-shop-srvs/user_srv/proto"
 	"net"
 
+	"github.com/hashicorp/consul/api"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
+	s := zap.S()
 	// flag处理的参数可在运行可执行文件时注入
 	IP := flag.String("ip", "0.0.0.0", "ip地址")
 	Port := flag.Int("port", 50051, "端口号")
@@ -22,8 +28,8 @@ func main() {
 	initialize.InitDB()
 
 	flag.Parse()
-	fmt.Println("ip", *IP)
-	fmt.Println("port", *Port)
+	s.Infof("ip", *IP)
+	s.Infof("port", *Port)
 
 	server := grpc.NewServer()
 	proto.RegisterUserServer(server, &handler.UserServer{})
@@ -32,8 +38,50 @@ func main() {
 		panic("failed to listen:" + err.Error())
 	}
 
+	// 注册服务健康检查
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+	err = Register()
+	if err != nil {
+		s.Errorf("注册健康检查时发生了错误")
+	}
+
 	err = server.Serve(lis)
 	if err != nil {
 		panic("failed to start grpc!" + err.Error())
 	}
+}
+
+// 将此服务注册到服务健康检查中心consul
+func Register() error {
+	conf := api.DefaultConfig()
+	conf.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host, global.ServerConfig.ConsulInfo.Port)
+
+	client, err := api.NewClient(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	var localHost string = "192.168.1.106"
+	var localPort int = 50051
+
+	check := api.AgentServiceCheck{
+		GRPC:                           fmt.Sprintf("%s:%d", localHost, localPort),
+		Timeout:                        "5s",
+		Interval:                       "5s",
+		DeregisterCriticalServiceAfter: "10s",
+	}
+
+	registration := new(api.AgentServiceRegistration)
+	registration.Address = localHost
+	registration.Port = localPort
+	registration.ID = global.ServerConfig.Name
+	registration.Name = global.ServerConfig.Name
+	registration.Tags = []string{"lele", "user", "srv"}
+	registration.Check = &check
+
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		panic(err)
+	}
+	return nil
 }
