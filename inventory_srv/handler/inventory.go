f53d82d405c254ab0ff1d8311da8e2f2b9bcfe30
wanglele <wanglele@luojilab.com>
2026-01-31 11:27:46 +0800

    Done inventory api

--- inventory_srv/handler/inventory.go
+++ inventory_srv/handler/inventory.go
@@ -2,10 +2,13 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"mx-shop-srvs/inventory_srv/global"
 	"mx-shop-srvs/inventory_srv/model"
 	"mx-shop-srvs/inventory_srv/proto"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"gorm.io/gorm"
 )
@@ -48,14 +51,58 @@ func (s *InventoryServer) SetInv(ctx context.Context, req *proto.GoodInvInfo) (*
 	return &emptypb.Empty{}, nil
 }
 
-// func (s *InventoryServer) InvDetail(ctx context.Context, req *proto.GoodInvInfo) (*proto.GoodInvInfo, error) {
+func (s *InventoryServer) InvDetail(ctx context.Context, req *proto.GoodInvInfo) (*proto.GoodInvInfo, error) {
+	var inventory model.Inventory
+	var resp proto.GoodInvInfo
+	result := global.DB.Where("good = ?", req.GoodId).Find(&inventory)
+	if result.Error != nil {
+		return nil, status.Errorf(codes.NotFound, "库存信息不存在")
+	}
+	resp.GoodId = inventory.Good
+	resp.Num = inventory.Stock
+	return &resp, nil
+}
 
-// }
+// 这里会有两个问题
+/*
+	1. 事务：如果用户要下单三个商品分别为：商品1购买1件，商品2购买两件，商品3购买3件，如果不做任何处理则会发生第一件商品库存扣减，第二个商品库存不足。
+	正确逻辑应为：全部扣减成功或者全部扣减失败，使用事务来解决这个问题。
+	2. 锁：不做处理高并发场景下会发生超卖情况，例如有两个请求同时对库存为1的商品进行扣减，这需要使用锁来解决。
+*/
+func (s *InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*proto.MsgTips, error) {
+	tx := global.DB.Begin()
+	for _, goodInfo := range req.GoodsInfo {
+		var inventory model.Inventory
+		result := global.DB.Where("good = ?", goodInfo.GoodId).Find(&inventory)
+		if result.RowsAffected == 0 {
+			tx.Rollback()
+			return nil, status.Errorf(codes.NotFound, fmt.Sprintf("找不到%s商品库存信息", goodInfo.GoodId))
+		}
+		if inventory.Stock < goodInfo.Num {
+			tx.Rollback()
+			return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("%s商品库存信息不足", goodInfo.GoodId))
+		}
+		inventory.Stock -= goodInfo.Num
+		tx.Save(&inventory)
+	}
+	tx.Commit()
+	return &proto.MsgTips{
+		Msg: "success",
+	}, nil
+}
 
-// func (s *InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*proto.MsgTips, error) {
-
-// }
-
-// func (s *InventoryServer) Reback(ctx context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
-
-// }
+func (s *InventoryServer) Reback(ctx context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
+	tx := global.DB.Begin()
+	for _, goodInfo := range req.GoodsInfo {
+		var inventory model.Inventory
+		result := global.DB.Where("good = ?", goodInfo.GoodId).Find(&inventory)
+		if result.RowsAffected == 0 {
+			tx.Rollback()
+			return nil, status.Errorf(codes.NotFound, fmt.Sprintf("找不到%s商品库存信息", goodInfo.GoodId))
+		}
+		inventory.Stock += goodInfo.Num
+		tx.Save(&inventory)
+	}
+	tx.Commit()
+	return &emptypb.Empty{}, nil
+}
--- inventory_srv/tests/main.go
+++ inventory_srv/tests/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"mx-shop-srvs/inventory_srv/model"
 	"mx-shop-srvs/inventory_srv/proto"
 
 	"google.golang.org/grpc"
@@ -23,16 +24,51 @@ func init() {
 }
 
 func main() {
-	TestCreateUpdateInventory()
 	conn.Close()
 }
 
-func TestCreateUpdateInventory() {
+func TestCreateUpdateInventory(id int32, num int32) {
 	_, err := inventoryClient.SetInv(context.Background(), &proto.GoodInvInfo{
-		GoodId: 432,
-		Num:    6,
+		GoodId: id,
+		Num:    num,
 	})
 	if err != nil {
 		panic(err)
 	}
+}
+
+func TestInvDetail() {
+	resp, err := inventoryClient.InvDetail(context.Background(), &proto.GoodInvInfo{
+		GoodId: 433,
+	})
+	if err != nil {
+		panic(err)
+	}
+	model.ToStringLog(resp)
+}
+
+func TestSell() {
+	resp, err := inventoryClient.Sell(context.Background(), &proto.SellInfo{
+		GoodsInfo: []*proto.GoodInvInfo{
+			&proto.GoodInvInfo{GoodId: 430, Num: 1},
+			&proto.GoodInvInfo{GoodId: 431, Num: 2},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	model.ToStringLog(resp)
+}
+
+func TestRollback() {
+	resp, err := inventoryClient.Reback(context.Background(), &proto.SellInfo{
+		GoodsInfo: []*proto.GoodInvInfo{
+			&proto.GoodInvInfo{GoodId: 431, Num: 2},
+			&proto.GoodInvInfo{GoodId: 430, Num: 1},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	model.ToStringLog(resp)
 }
