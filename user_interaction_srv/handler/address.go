c0a2459894261c4bb367f8947f289110f5dffe20
wanglele <wanglele@luojilab.com>
2026-03-19 00:07:20 +0800

    Add address api

--- user_interaction_srv/handler/address.go
+++ user_interaction_srv/handler/address.go
@@ -1,8 +1,14 @@
 package handler
 
 import (
+	"context"
+	"mx-shop-srvs/user_interaction_srv/global"
+	"mx-shop-srvs/user_interaction_srv/model"
 	"mx-shop-srvs/user_interaction_srv/proto"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"gorm.io/gorm"
 )
 
@@ -26,4 +32,85 @@ func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
 		offset := (page - 1) * pageSize
 		return db.Offset(offset).Limit(pageSize)
 	}
+}
+
+func (s *AddressServer) GetAddressList(ctx context.Context, req *proto.AddressRequest) (*proto.AddressListResponse, error) {
+	var addresses []model.Address
+
+	if result := global.DB.Where("user = ?", req.UserId).Find(&addresses); result.Error != nil {
+		return nil, status.Errorf(codes.Internal, "查询地址信息时出错了")
+	}
+
+	var resp proto.AddressListResponse
+	resp.Total = int32(len(addresses))
+
+	for _, address := range addresses {
+		resp.Data = append(resp.Data, &proto.AddressResponse{
+			Id:           address.ID,
+			UserId:       address.User,
+			Province:     address.Province,
+			City:         address.City,
+			District:     address.District,
+			Address:      address.Address,
+			SignerName:   address.SignerName,
+			SignerMobile: address.SignerMobile,
+		})
+	}
+
+	return &resp, nil
+}
+
+func (s *AddressServer) CreateAddress(ctx context.Context, req *proto.AddressRequest) (*proto.AddressResponse, error) {
+	address := model.Address{
+		User:         req.UserId,
+		Province:     req.Province,
+		City:         req.City,
+		District:     req.District,
+		Address:      req.Address,
+		SignerName:   req.SignerName,
+		SignerMobile: req.SignerMobile,
+		BaseModel:    model.BaseModel{},
+	}
+
+	result := global.DB.Create(&address)
+	if result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.Internal, "新建地址时出错了")
+	}
+
+	return &proto.AddressResponse{
+		Id: address.ID,
+	}, nil
+}
+
+func (s *AddressServer) DeleteAddress(ctx context.Context, req *proto.AddressRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Where("id = ? and user = ?", req.Id, req.UserId).Find(&model.Address{}); result.RowsAffected == 0 {
+		return &emptypb.Empty{}, status.Errorf(codes.NotFound, "找不到改地址信息")
+	}
+
+	if result := global.DB.Where("id = ? and user = ?", req.Id, req.UserId).Delete(&model.Address{}); result.RowsAffected == 0 {
+		return &emptypb.Empty{}, status.Errorf(codes.Internal, "删除失败")
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *AddressServer) UpdateAddress(ctx context.Context, req *proto.AddressRequest) (*emptypb.Empty, error) {
+	var address model.Address
+	result := global.DB.Where("id = ? and user = ?", req.Id, req.UserId).Find(&address)
+	if result.RowsAffected == 0 {
+		return &emptypb.Empty{}, status.Errorf(codes.NotFound, "找不到改地址信息")
+	}
+
+	address.Province = req.Province
+	address.City = req.City
+	address.District = req.District
+	address.Address = req.Address
+	address.SignerMobile = req.SignerMobile
+	address.SignerName = req.SignerName
+
+	if result := global.DB.Save(&address); result.RowsAffected == 0 {
+		return &emptypb.Empty{}, status.Errorf(codes.NotFound, "更新地址信息失败")
+	}
+
+	return &emptypb.Empty{}, nil
 }
--- user_interaction_srv/tests/main.go
+++ user_interaction_srv/tests/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"mx-shop-srvs/user_interaction_srv/model"
 	"mx-shop-srvs/user_interaction_srv/proto"
 
 	"google.golang.org/grpc"
@@ -13,15 +15,81 @@ var (
 	conn          *grpc.ClientConn
 )
 
-// func init() {
-// 	var err error
-// 	conn, err = grpc.Dial("127.0.0.1:50052", grpc.WithInsecure())
-// 	if err != nil {
-// 		panic(err)
-// 	}
+func init() {
+	var err error
+	conn, err = grpc.Dial("127.0.0.1:50054", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
 
-// 	inventoryClient = proto.NewInventoryClient(conn)
-// }
+	addressClient = proto.NewAddressClient(conn)
+}
 
 func main() {
+	// TestCreateAddress()
+	// TestUpdateAddress()
+	// TestAddressList()
+	// TestDeleteAddress()
+}
+
+func TestCreateAddress() {
+	resp, err := addressClient.CreateAddress(context.Background(), &proto.AddressRequest{
+		UserId:       10,
+		Province:     "河北省",
+		City:         "邯郸市",
+		District:     "曲周县",
+		Address:      "候村镇龙李庄村105号",
+		SignerName:   "李哈哈",
+		SignerMobile: "16632021639",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	model.ToStringLog(resp)
+}
+
+func TestUpdateAddress() {
+	resp, err := addressClient.UpdateAddress(context.Background(), &proto.AddressRequest{
+		Id:           1,
+		UserId:       10,
+		Province:     "河北省",
+		City:         "邯郸市",
+		District:     "曲周县",
+		Address:      "安寨镇龙李庄村105号",
+		SignerName:   "李哈哈",
+		SignerMobile: "16632021639",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	model.ToStringLog(resp)
+}
+
+func TestAddressList() {
+	resp, err := addressClient.GetAddressList(context.Background(), &proto.AddressRequest{
+		UserId: 10,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	model.ToStringLog(resp)
+}
+
+func TestDeleteAddress() {
+	resp, err := addressClient.DeleteAddress(context.Background(), &proto.AddressRequest{
+		Id:     1,
+		UserId: 10,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	model.ToStringLog(resp)
 }
